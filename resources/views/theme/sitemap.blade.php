9c5c46e3796e7a28e6aa9cb4c50122c6a0075830
Don Jones <newelementdesigns@me.com>
2020-05-21 14:21:42 -0400

    Fixed date error

--- resources/views/theme/sitemap.blade.php
+++ resources/views/theme/sitemap.blade.php
@@ -4,10 +4,16 @@
     @foreach( $objects as $object )
     @php
         $data = _getObjectSitemapData($type, $object, $sitemap_settings);
+        //$date = now()->timezone( config('neutrino.timezone') )->format('Y-m-d');
+        if ( $object->updated_at ){
+            $date = $object->updated_at->timezone( config('neutrino.timezone') )->format('Y-m-d');
+        } else if( !$object->updated_at && $object->created_at ){
+            $date = $object->created_at->timezone( config('neutrino.timezone') )->format('Y-m-d');
+        }
     @endphp
     <url>
         <loc>{{ config('app.url') }}{{ $data['url'] }}</loc>
-        <lastmod>{{ $object->updated_at->timezone( config('neutrino.timezone') )->format('Y-m-d') }}</lastmod>
+        <lastmod>{{ $date }}</lastmod>
         <changefreq>{{ $object->sitemap_change? $object->sitemap_change : $data['change'] }}</changefreq>
         <priority>{{ $object->sitemap_priority? $object->sitemap_priority : $data['priority'] }}</priority>
     </url>
